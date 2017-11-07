424b345217525d0d6b1678ee239a966844f2215d
Benjamin de Bos <benjamindebos@usenetbucket.com>
2017-11-07 23:28:45 +0100

    WIF update

--- lib/Crypto/WIF.php
+++ lib/Crypto/WIF.php
@@ -3,22 +3,19 @@ namespace NeoPHP\Crypto;
 
 use NeoPHP\Crypto\Base58;
 
-
 //taken from: https://en.bitcoin.it/wiki/Wallet_import_format
 
 
 class WIF {
 	
-	public static function createWifFromPrivateKey($privateKey) {
-		return array(
-			"private_key_hex" => bin2hex($privateKey),
-			"private_key_compressed" => "120".bin2hex($privateKey)."01",
-			"wif" => Base58::checkEncode(0x80, $privateKey, 0x01)
-		);
+	public static function createWifFromPrivateKey($privateKey) {	
+		return Base58::checkEncode(128, hex2bin($privateKey), true);
 	}
 	
+/*
 	public static function getPrivateKeyFromWif($wif) {
 		return Base58::decode($wif);
 	}
+*/
 	
 }
