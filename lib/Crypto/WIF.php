09bbf021df652a9448c4db9b388cb13df35247bb
Benjamin de Bos <benjamindebos@usenetbucket.com>
2017-11-08 23:26:55 +0100

    Added private key from WIF

--- lib/Crypto/WIF.php
+++ lib/Crypto/WIF.php
@@ -8,14 +8,51 @@ use NeoPHP\Crypto\Base58;
 
 class WIF {
 	
+	/**
+	 * createWifFromPrivateKey function.
+	 * 
+	 * @access public
+	 * @static
+	 * @param mixed $privateKey
+	 * @return void
+	 */
 	public static function createWifFromPrivateKey($privateKey) {	
-		return Base58::checkEncode(128, hex2bin($privateKey), true);
+		return Base58::checkEncode($privateKey);
 	}
 	
-/*
+	
+	/**
+	 * getPrivateKeyFromWif function.
+	 * 
+	 * @access public
+	 * @static
+	 * @param mixed $wif
+	 * @return void
+	 */
 	public static function getPrivateKeyFromWif($wif) {
-		return Base58::decode($wif);
+		return Base58::checkDecode($wif);
 	}
-*/
+	
+	
+
+	/**
+	 * validateWif function.
+	 * 
+	 * @access public
+	 * @static
+	 * @param mixed $wif
+	 * @return void
+	 */
+	public static function validateWif($wif) {
+	    
+	    //validate the WIF
+	    $uncompressedWif = BcMathUtils::bc2bin(Base58::decode($wif));
+	    
+	    //filter out last 4 bytes
+		$uncompressedWifNoChecksum = substr($uncompressedWif, 0, -4);
+		
+		//check if the last 4 bytes wwith the first four of the uncompressed wif, with SHA256 twice
+		return (substr($uncompressedWif, -4) == substr(Hash::SHA256(Hash::SHA256($uncompressedWifNoChecksum)), 0, 4));
+	}
 	
 }
