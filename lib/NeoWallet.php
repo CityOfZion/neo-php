751bf44546b044cd6abc485b6cc063b8d6e3c03f
Benjamin de Bos <benjamindebos@usenetbucket.com>
2017-11-08 23:27:02 +0100

    Added Wif validation

--- lib/NeoWallet.php
+++ lib/NeoWallet.php
@@ -2,6 +2,11 @@
 namespace NeoPHP;
 
 class NeoWallet {
+
+	
+	private $privateKeyHex = null,$wif = null, $publicKeyHex = null;
+
+    	
 	
 	/**
 	 * newNeoWallet function.
@@ -9,22 +14,63 @@ class NeoWallet {
 	 * @access public
 	 * @return void
 	 */
-	public static function newNeoWallet() {
-		//Create a simple array privatekey
-		$privateKeyHex = Crypto\KeyPair::createPrivateKeyHex();
-		$publicKeyHex  = Crypto\KeyPair::getPublicKeyFromPrivateKey($privateKeyHex);
-
+	public function __construct($walletWif = false) {
 		
-		//get the WIF from the private key
-		$neoWallet = array(
-			"wif"=>Crypto\WIF::createWifFromPrivateKey($privateKeyHex),
- 			"private_key_hex" => $privateKeyHex,
-			"public_key_hex" => $publicKeyHex
-//			"address" => self::getAddressFromPublicKey($publicKeyHex),
-		);
-		
-		return $neoWallet;
+		if (!$walletWif) {
+			$this->privateKeyHex = Crypto\KeyPair::createPrivateKeyHex();
+			$this->wif = Crypto\WIF::createWifFromPrivateKey($this->privateKeyHex);
+		} else {
+			if (Crypto\WIF::validateWif($walletWif)) {
+				$this->privateKeyHex = Crypto\WIF::getPrivateKeyFromWif($walletWif);
+				$this->wif = $walletWif;
+			} else {
+				throw new \Exception("Invalid WIF");
+			}
+		}
+		return $this;
 	}
+	
+	
+	/**
+	 * getPrivateKey function.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function getPrivateKey() {
+		return $this->privateKeyHex;
+	}
+
+
+	public function getPublicKey() {
+		
+		if (!isset($this->wif))
+			throw new Exception("No WIF set");
+
+		if (!isset($this->privateKeyHex))
+			throw new Exception("No private key set");
+
+		if (!isset($this->publicKeyHex))
+			$this->publicKeyHex = Crypto\KeyPair::getPublicKeyFromPrivateKey($this->privateKeyHex);
+			
+		return $this->publicKeyHex;
+	}
+	
+	public function getWIF() {
+		if (!isset($this->wif))
+			$this->wif = Crypto\WIF::createWifFromPrivateKey($this->privateKeyHex);
+
+		return $this->wif;
+	}
+	
+	public function getAddress() {
+		if (!isset($this->wif))
+			throw new Exception("No WIF set");
+		
+		self::getAddressFromPublicKey($publicKeyHex);
+	}
+	
+	
 	
 
 
