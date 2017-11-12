7d93542b9dbc7aab9323f80d8bc515d397e60df7
Benjamin de Bos <benjamindebos@usenetbucket.com>
2017-11-12 21:41:46 +0100

    Create wallet example

--- examples/create-wallet-encrypted.php
+++ examples/create-wallet-encrypted.php
@@ -16,26 +16,7 @@ _require_all("./../src/");
 
 //New wallet
 $newWallet = new NeoPHP\NeoWallet();
-
-/*
-print_r([
-    "isNEP2" => (($newWallet->isNEP2()) ? "🔒" : "🚫"),
-    "wif" => $newWallet->getWif(),
-    "address" => $newWallet->getAddress(),
-    "private_key" => $newWallet->getPrivateKey(),
-    "public_key" => $newWallet->getPublicKey()
-]);
-*/
-
 $newWallet->encryptWallet("test1234");
-
-/*
-echo "\n";
-echo $newWallet->getEncryptedKey();
-echo "\n";
-die();
-*/
-
 print_r([
     "isNEP2" => (($newWallet->isNEP2()) ? "🔒" : "🚫"),
     "encryptedKey" => $newWallet->getEncryptedKey(),
