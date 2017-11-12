8b67cb2c2cd00f93119d4be4cc6193bddd72c608
Benjamin de Bos <benjamindebos@usenetbucket.com>
2017-11-12 01:34:44 +0100

    Added NEP2 decryption

---
+++ examples/open-wallet-encrypted.php
@@ -0,0 +1,24 @@
+<?php
+	function _require_all($dir) {
+        // require all php files
+        $scan = glob("$dir/*");
+        foreach ($scan as $path) {
+            if (preg_match('/\.php$/', $path)) {
+                require_once $path;
+            }
+            elseif (is_dir($path)) {
+                _require_all($path);
+            }
+        }
+    }
+    _require_all("./../src/");
+
+	//Existing wallet encrypted with NEP2
+	$existingWallet = new NeoPHP\NeoWallet("6PYPUvTG4a5cpCEha4ew6GSx2WDsKnu2CQb3sVwkgzvnsGRt4gyYw9roaf","test1234");
+	print_r(array(
+		"isNep2" => (($existingWallet->isNep2()) ? "🔒" : "🚫"),
+		"wif" => $existingWallet->getWif(),
+		"address" => $existingWallet->getAddress(),
+		"private_key" => $existingWallet->getPrivateKey(),
+		"public_key" => $existingWallet->getPublicKey()
+	));
