8e550a07d89ed04b55eee331273351e73b0029bd
Benjamin de Bos <info@its-vision.nl>
2018-03-02 16:55:52 +0100

    Added string2hex functions

---
+++ src/Tools/StringTools.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace NeoPHP\Tools;
+
+class StringTools
+{
+	
+	/**
+	 * string2hex function.
+	 * 
+	 * @access public
+	 * @static
+	 * @param mixed $string
+	 * @return void
+	 */
+	public static function string2hex($string){
+	    $hex='';
+	    for ($i=0; $i < strlen($string); $i++){
+	        $hex .= dechex(ord($string[$i]));
+	    }
+	    return $hex;
+	}
+	
+	/**
+	 * hex2string function.
+	 * 
+	 * @access public
+	 * @static
+	 * @param mixed $hex
+	 * @return void
+	 */
+	public static function hex2string($hex){
+	    $string='';
+	    for ($i=0; $i < strlen($hex)-1; $i+=2){
+	        $string .= chr(hexdec($hex[$i].$hex[$i+1]));
+	    }
+	    return $string;
+	}
+	
+	public static function uInt16($i, $endianness=false) {
+        $f = is_int($i) ? "pack" : "unpack";
+
+        if ($endianness === true) {  // big-endian
+            $i = $f("n", $i);
+        }
+        else if ($endianness === false) {  // little-endian
+            $i = $f("v", $i);
+        }
+        else if ($endianness === null) {  // machine byte order
+            $i = $f("S", $i);
+        }
+
+        return is_array($i) ? $i[1] : $i;
+    }
+    
+
+        
+        	
+}	
